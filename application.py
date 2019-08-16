400b83970bb3519acaec985d8712d85f378e9afa
joejoe2 <39475537+joejoe2@users.noreply.github.com>
2019-08-16 12:42:47 +0800

    Add files via upload

---
+++ application.py
@@ -0,0 +1,39 @@
+import json
+from flask import Flask, request
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+line_bot_api = LineBotApi('uJ6LnR2TWpOmYqSYW9yg1nSe1GO9+c3euzndfcjTeTSlfz1r58dfBxpnNySHjq7/oUAmiO0VBDHb5HyLSdnu+PXMA71HJkbT4jwqfTDXhpXRAWOFsEVbpjbDNahvbXVLVh9cZsTLH+yB3cxwMsyC/gdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('905c2e3f57145acb7d791c46dec9a573')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<p>Hello World!</p>"
+
+
+@app.route('/', methods=['POST'])
+def callback():
+    json_line = request.get_json()
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    user = decoded['result'][0]['content']['from']
+    text = decoded['result'][0]['content']['text']
+    print(json_line)
+    #print("使用者：", user)
+    #print("內容：", text)
+    #sendText(user, text)
+    return ''
+
+
+if __name__ == '__main__':
+    app.run()
